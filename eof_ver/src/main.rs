aec9fa89927540e79fbc6b1ee100d1946d6231d9
rtecnica <imaldonado@ing.uchile.cl>
2022-01-07 20:51:34 -0300

    Minor changes

--- eof_ver/src/main.rs
+++ eof_ver/src/main.rs
@@ -1,5 +1,11 @@
-use std::io;
+use std::fs;
+use std::env;
 
-fn main() {               
-    println!("Hello, world!");
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    
+    println!("{}", &args[1]);
+    let file = fs::read(&args[1]).unwrap();
+    println!("{:?}", file);
+    // Turns out rust doesn't handle files in the same way as C  
 }
