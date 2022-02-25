629e590a1905181d3ee026fde93921f87dc8202d
rtecnica <imaldonado@ing.uchile.cl>
2022-02-24 22:54:34 -0300

    Formatted c files according to clangd

--- cross_ref/src/llist.c
+++ cross_ref/src/llist.c
@@ -7,134 +7,135 @@
  */
 
 #include "../include/llist.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 struct llist llist_init() {
-    struct llist list;
-    list.length = 0;
-    list.head = NULL;
-    list.tail = NULL;
-    return list;
-}    
+  struct llist list;
+  list.length = 0;
+  list.head = NULL;
+  list.tail = NULL;
+  return list;
+}
 
 void llist_deinit(struct llist list) {
-    void *ptr = llist_pop(&list, head);
-    while(ptr != NULL) {
-        free(ptr);
-        ptr = llist_pop(&list, head);
-    }
-    
-    if(list.head == NULL && list.tail == NULL) {
-        return;
-    } else {
-        printf("Something went wrong!");
-    }
+  void *ptr = llist_pop(&list, head);
+  while (ptr != NULL) {
+    free(ptr);
+    ptr = llist_pop(&list, head);
+  }
+
+  if (list.head == NULL && list.tail == NULL) {
+    return;
+  } else {
+    printf("Something went wrong!");
+  }
 }
 
 void llist_push(struct llist *list, void *item) {
-    struct lnode *new = malloc(sizeof(lnode));
-    new->item = item;
-    new->next = NULL;
-    
-    if(list->length > 0) {
-        list->tail->next = new;
-        list->tail = new;
-    } else {
-        list->tail = new;
-        list->head = new;
-    }
-    list->length++;
+  struct lnode *new = malloc(sizeof(lnode));
+  new->item = item;
+  new->next = NULL;
+
+  if (list->length > 0) {
+    list->tail->next = new;
+    list->tail = new;
+  } else {
+    list->tail = new;
+    list->head = new;
+  }
+  list->length++;
 }
 
 void *llist_pop(struct llist *list, llside side) {
-    void *item;
-    struct lnode *old = NULL;
-    
-    if(list->length < 1) {
-        return NULL;
-    } else {
-        switch(side) {
-            case head:
-                old = list->head;
-                list->head = old->next;
-                break;
-         
-            case tail:
-                old = list->tail;
-                struct lnode *new_tail = list->head;
-                while(new_tail->next != old && new_tail->next != NULL) {
-                    new_tail = new_tail->next;
-                }
-                list->tail = new_tail;
-                break;
-        }
-    }
+  void *item;
+  struct lnode *old = NULL;
 
-    list->length--;
-    item = old->item;
-   
-    if(list->head == NULL || list->tail == NULL) {
-        list->head = NULL;
-        list->tail = NULL;
-    }
+  if (list->length < 1) {
+    return NULL;
+  } else {
+    switch (side) {
+    case head:
+      old = list->head;
+      list->head = old->next;
+      break;
 
-    if(old != NULL) {
-        free(old);
+    case tail:
+      old = list->tail;
+      struct lnode *new_tail = list->head;
+      while (new_tail->next != old && new_tail->next != NULL) {
+        new_tail = new_tail->next;
+      }
+      list->tail = new_tail;
+      break;
     }
-    
-    return item;
+  }
+
+  list->length--;
+  item = old->item;
+
+  if (list->head == NULL || list->tail == NULL) {
+    list->head = NULL;
+    list->tail = NULL;
+  }
+
+  if (old != NULL) {
+    free(old);
+  }
+
+  return item;
 }
 
-void *llist_select(struct llist *list, void* item, int (*compare_fn)(void *item1, void *item2)) {
-    struct lnode *ptr = list->head;
-   
-    while(ptr != NULL) {
-        if(compare_fn(ptr->item, item) == 0) {
-            break;  
-        }
-        ptr = ptr->next;
+void *llist_select(struct llist *list, void *item,
+                   int (*compare_fn)(void *item1, void *item2)) {
+  struct lnode *ptr = list->head;
+
+  while (ptr != NULL) {
+    if (compare_fn(ptr->item, item) == 0) {
+      break;
     }
-    
-    if(ptr == NULL) {
-        return NULL;
-    } else {
-        return ptr->item;
-    }
+    ptr = ptr->next;
+  }
+
+  if (ptr == NULL) {
+    return NULL;
+  } else {
+    return ptr->item;
+  }
 }
 
 // PROTIP: Sort linked lists by turning them into arrays first
-void llist_bubble_sort(struct llist *list, int (*compare_fn)(void *item1, void *item2)) { 
-    struct lnode **array = malloc(sizeof(void*)*list->length);
-    struct lnode *ptr = list->head;
-    int i = 0;
-    while(ptr != NULL) {
-        array[i] = ptr;
-        ptr = ptr->next;
-        i++;
-    }
-    
-    int swapped_this_pass = 1;
-    while(swapped_this_pass) {
-        swapped_this_pass = 0;
-        for(int j = 0; j < list->length-1; j++) {
-            if(compare_fn(array[j]->item, array[j+1]->item) == 1) {
-                struct lnode *temp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = temp;
-                swapped_this_pass = 1;
-            }
-        }
-    }
+void llist_bubble_sort(struct llist *list,
+                       int (*compare_fn)(void *item1, void *item2)) {
+  struct lnode **array = malloc(sizeof(void *) * list->length);
+  struct lnode *ptr = list->head;
+  int i = 0;
+  while (ptr != NULL) {
+    array[i] = ptr;
+    ptr = ptr->next;
+    i++;
+  }
 
-    list->head = array[0];
-    ptr = list->head;
-    for(int j = 1; j < list->length; j++) {
-        ptr->next = array[j];
-        ptr = ptr->next;
+  int swapped_this_pass = 1;
+  while (swapped_this_pass) {
+    swapped_this_pass = 0;
+    for (int j = 0; j < list->length - 1; j++) {
+      if (compare_fn(array[j]->item, array[j + 1]->item) == 1) {
+        struct lnode *temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+        swapped_this_pass = 1;
+      }
     }
-    ptr->next = NULL;
+  }
 
-    free(array);
+  list->head = array[0];
+  ptr = list->head;
+  for (int j = 1; j < list->length; j++) {
+    ptr->next = array[j];
+    ptr = ptr->next;
+  }
+  ptr->next = NULL;
 
+  free(array);
 }
--- cross_ref/src/word_parse.c
+++ cross_ref/src/word_parse.c
@@ -7,87 +7,86 @@
  */
 
 #include "../include/word_parse.h"
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int compare_word_alph(void *item1, void *item2) {
-    struct word_t *word1 = (struct word_t *) item1;
-    struct word_t *word2 = (struct word_t *)item2;
-    for(int i = 0; i < MAX_WORD_LEN; i++) {
-        int expr = (word1->word[i] > word2->word[i]) - (word1->word[i] < word2->word[i]);
-        if(word1->word[i] == '\0' || word2->word[i] == '\0') {
-            return -expr; 
-        }
-        switch(expr) {
-            case 1:
-                return 1;
-                break;
-            case -1:
-                return -1;
-                break;
-            default:
-                break;
-        }
+  struct word_t *word1 = (struct word_t *)item1;
+  struct word_t *word2 = (struct word_t *)item2;
+  for (int i = 0; i < MAX_WORD_LEN; i++) {
+    int expr =
+        (word1->word[i] > word2->word[i]) - (word1->word[i] < word2->word[i]);
+    if (word1->word[i] == '\0' || word2->word[i] == '\0') {
+      return -expr;
     }
-    return 0;
+    switch (expr) {
+    case 1:
+      return 1;
+      break;
+    case -1:
+      return -1;
+      break;
+    default:
+      break;
+    }
+  }
+  return 0;
 }
 
 int compare_word_count(void *item1, void *item2) {
-    struct word_t *word1 = (struct word_t *) item1;
-    struct word_t *word2 = (struct word_t *)item2;
-    return (int)((word1->count < word2->count) - (word1->count > word2->count));
+  struct word_t *word1 = (struct word_t *)item1;
+  struct word_t *word2 = (struct word_t *)item2;
+  return (int)((word1->count < word2->count) - (word1->count > word2->count));
 }
 
 struct word_t create_word(char *word) {
-    struct word_t new;
-    int i = 0;
-    char c = word[i];
-    while(c != '\0' || i > MAX_WORD_LEN) {
-        new.word[i] = tolower(c);
-        i++;
-        c = word[i];
-    }
-    new.word[i] = '\0';
-    new.count = 0;
-    new.pages = llist_init();
-    return new;
+  struct word_t new;
+  int i = 0;
+  char c = word[i];
+  while (c != '\0' || i > MAX_WORD_LEN) {
+    new.word[i] = tolower(c);
+    i++;
+    c = word[i];
+  }
+  new.word[i] = '\0';
+  new.count = 0;
+  new.pages = llist_init();
+  return new;
 }
 
 int compare_pages(void *page1, void *page2) {
-    return (int)((*(int*)page1 < *(int*)page2) - (*(int*)page1 > *(int*)page2));
+  return (int)((*(int *)page1 < *(int *)page2) -
+               (*(int *)page1 > *(int *)page2));
 }
 
 void word_add_page(struct word_t *word, int page) {
-    int *p = malloc(sizeof(int));
-    *p = page;
-    if(llist_select(&(word->pages), p, &compare_pages) == NULL) { 
-        llist_push(&(word->pages), p);
-    } else {
-        free(p);
-    }
+  int *p = malloc(sizeof(int));
+  *p = page;
+  if (llist_select(&(word->pages), p, &compare_pages) == NULL) {
+    llist_push(&(word->pages), p);
+  } else {
+    free(p);
+  }
 }
 
-void deinit_word(struct word_t *word) {
-    llist_deinit((word->pages));
-}
+void deinit_word(struct word_t *word) { llist_deinit((word->pages)); }
 
 void print_word(struct word_t *word) {
-    printf("%s:\t\t", word->word);
-    printf("%d\tp: ", word->count);
-    struct lnode *ptr = word->pages.head;
-    while(ptr != NULL) {
-       printf("%d", *(int*)ptr->item);
-       ptr = ptr->next;
-       if(ptr != NULL) {
-           printf(", ");
-       } else {
-           printf("\n");
-           return;
-       }
+  printf("%s:\t\t", word->word);
+  printf("%d\tp: ", word->count);
+  struct lnode *ptr = word->pages.head;
+  while (ptr != NULL) {
+    printf("%d", *(int *)ptr->item);
+    ptr = ptr->next;
+    if (ptr != NULL) {
+      printf(", ");
+    } else {
+      printf("\n");
+      return;
     }
-    printf("none\n");
-    return;
+  }
+  printf("none\n");
+  return;
 }
-
